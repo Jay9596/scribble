ed14c68f2c9ae63c77e6e00901dd6cce0e4ccd34
Jay Shah <jayshah.9596@gmail.com>
2017-11-29 01:19:31 +0530

    Basic operations :hammer: - post - get - patch / put - delete / del - help

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,115 @@
+use std::env;
+use std::fs;
+use std::fs::{File, OpenOptions};
+use std::path::Path;
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => print_usage(),
+        2 => {
+            let title = &args[1];
+           match &title[..] {
+               "get" | "post" | "patch" | "delete" => {},
+               _ => {
+                   println!("Fetching note :{}", title);
+                   get_note(title);
+               },
+           }
+        }
+        3 => {
+            if &args[1] == "get"{
+                println!("Fetching note :{}", &args[2]);
+                get_note(&args[2]);
+            }else if &args[1] == "delete" || &args[1] == "del" {
+                println!("Deleting {}",&args[2]);
+                delete_note(&args[2]);
+            }else{
+                print_usage()
+            }
+        },
+        4 => {
+
+                let title = &args[2];
+                let text = &args[3];
+            if &args[1] == "post" {
+                println!("Posting new note {}",title);
+                post_note(title, text);
+            } else if &args[1] == "patch" {
+                println!("Patching note {}", title);
+                patch_note(title, text);
+            }else {
+                print_usage();
+            }
+        }
+        _ => {}
+    }
+}
+
+fn get_note(title: &str) {
+    let file_name = get_file_name(title);
+    let path = Path::new(file_name.as_str());
+    // println!("path {}", path.display());
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Error: {}", why),
+        Ok(file) => file
+    };
+    let mut text = String::new();
+    file.read_to_string(&mut text).unwrap();
+    println!("Text:\n{}", text);
+}
+
+fn post_note(title: &str, text: &str) {
+    let file_name = get_file_name(title);
+    let path = Path::new(file_name.as_str());
+    println!("path {}", path.display());
+    let mut file = match OpenOptions::new()
+                            .create(true)
+                            .write(true)
+                            .truncate(true)
+                            .open(&path){
+        Ok(file) => file,
+        Err(why) => panic!("Could not create file {}", why),
+    };
+    let _ = file.write(text.as_bytes()).unwrap();
+
+}
+
+fn patch_note(title: &str, text: &str) {
+    let file_name = get_file_name(title);
+    let path = Path::new(file_name.as_str());
+    // println!("path {}", path.display());
+    let mut file = match OpenOptions::new()
+                            .read(true)
+                            .append(true)
+                            .open(&path) {
+        Err(why) => panic!("Error: {}", why),
+        Ok(file) => file
+    };
+    let _ = file.write_all(text.as_bytes()).unwrap();
+}
+
+fn delete_note(title: &str) {
+    let file_name = get_file_name(title);
+    let path = Path::new(file_name.as_str());
+    fs::remove_file(&path).unwrap();
+}
+
+fn get_file_name(title: &str) -> String {
+    let mut file_name = String::from(title);
+    file_name.push_str(".txt");
+    file_name
+}
+
+fn print_usage() {
+    println!("usage: 
+scribble <command> [<args>]\nscribble <title>\tPrints the note.
+
+Commands:
+post   <title> <text>\tWrites the text in the note.
+get    <title>       \tPrints the note.
+patch  <title> <text>\tUpdates <text> of note.
+delete <title>       \tDelete the note.
+help                 \tPrints help");
 }
